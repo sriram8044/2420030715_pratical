8d8494a530e0af125d606c48bb96b9d1e91e028f
sriram8044 <84059952+sriram8044@users.noreply.github.com>
2025-08-02 09:33:44 +0530

    Add files via upload

---
+++ data_types.java
@@ -0,0 +1,21 @@
+// Source code is decompiled from a .class file using FernFlower decompiler.
+public class data_types {
+
+
+   public static void main(String[] var0) {
+      byte var1 = 2;
+      float var2 = 3.0F;
+      char var3 = 'S';
+      double var4 = 33.054;
+      long var6 = 1234565L;
+      boolean var8 = true;
+      boolean var9 = false;
+      System.out.println(var1);
+      System.out.println(var2);
+      System.out.println(var3);
+      System.out.println(var6);
+      System.out.println(var4);
+      System.out.println(var8);
+      System.out.println(var9);
+   }
+}
