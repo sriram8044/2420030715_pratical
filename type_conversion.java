bb9ab1279348911e0c22f44e21e14f3f7d6dc91e
sriram8044 <84059952+sriram8044@users.noreply.github.com>
2025-08-02 09:34:24 +0530

    type conversion

---
+++ type_conversion.java
@@ -0,0 +1,11 @@
+class type_conversion{
+public static void main(String args[])
+	{
+		Integer a=554;
+		Boolean b=true;
+		double d=9.876d;
+		int c=10;
+		System.out.println(a.toString().length());
+		System.out.println(b.toString().length());
+	}
+}
